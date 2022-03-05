dc787b490bc746f9387c21c499cd3ad2276143d2
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-03-05 03:14:51 -0500

    Fin de la seccion 4

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Counter :start="100"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Counter from './components/Counter.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Counter
   }
 }
+
 </script>
 
 <style>
---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+    <h2>{{ customTitle }}</h2>
+    <p>{{ counter }}<sup>2</sup> = {{ squareCounter }}</p>
+
+    <div>
+        <button @click="decreaseValue">-1</button>
+        <button @click="increaseValue">+1</button>
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            // required: true,
+            default: 100,
+            validator( value ) {
+                return (value >= 100)
+            }
+        }
+    },
+    name: "Counter",
+    data() {
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        increaseValue() {
+            this.counter ++;
+        },
+        decreaseValue() {
+            this.counter --;
+        }
+    },
+    computed: {
+        squareCounter() {
+            return this.counter * this.counter;
+        },
+        customTitle() {
+           return (this.title) ? this.title : "Counter";
+        }
+    }
+}
+</script>
+
+<style>
+
+button {
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 10px 20px;
+    transition: 0.3s ease-in-out;
+}
+
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
+
+</style>
